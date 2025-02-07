9d153ef43b2a8573c6b66a5402e6493db29f0867
JasirBasheer <jasirbinbasheerpp@gmail.com>
2025-02-07 19:50:44 +0530

    fix: fixed errorHandler middleware

--- src/middleware/error.handler.ts
+++ src/middleware/error.handler.ts
@@ -2,12 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import { BaseError } from '../errors/error.base';
 
 export const errorHandler = (
-    err: any, 
-    req: Request, 
-    res: Response, 
+    err: any,
+    req: Request,
+    res: Response,
     next: NextFunction
 ): void => {
-    console.error('Error:', err.message || err);
+    res.locals.errorDetails = JSON.stringify({
+        message: err.message,
+        stack: err.stack,
+        timestamp: new Date().toISOString()
+    });
+
 
     if (err.message === "Invalid Token") {
         res.clearCookie('accessToken', { httpOnly: true, secure: false });
@@ -16,7 +21,7 @@ export const errorHandler = (
 
     if (err instanceof BaseError) {
         console.log(err);
-        res.status(err.statusCode).json({ error:err.message });
+        res.status(err.statusCode).json({ error: err.message });
     } else {
         res.status(500).json({
             message: err.message || "Something went wrong",
