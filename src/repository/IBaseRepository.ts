b45888acd58024eaf08b963f7018b9d53797b938
JasirBasheer <jasirbinbasheerpp@gmail.com>
2025-02-07 10:29:23 +0530

    feat: added base repository

---
+++ src/repository/IBaseRepository.ts
@@ -0,0 +1,9 @@
+import { Document, FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
+
+export interface IBaseRepository<T extends Document> {
+    find(query: FilterQuery<T>): Promise<T[] | []>;
+    findOne(query: FilterQuery<T>): Promise<T | null>;
+    create(data: Partial<T>): Promise<T | null>;
+    update(query: FilterQuery<T>, data: UpdateQuery<T>, option?: QueryOptions): Promise<T | null>;
+    delete(query: FilterQuery<T>): Promise<T | null>;
+}
---
+++ src/repository/baseRepository.ts
@@ -0,0 +1,56 @@
+import { Document, FilterQuery, Model, QueryOptions, UpdateQuery } from "mongoose";
+import { IBaseRepository } from "./IBaseRepository";
+
+export class BaseRepository<T extends Document> implements IBaseRepository<T> {
+    protected readonly model: Model<T>;
+
+    constructor(model: Model<T>) {
+        this.model = model;
+    }
+
+
+    async find(query: FilterQuery<T>): Promise<T[] | []> {
+        try {
+            return await this.model.find(query);
+        } catch (err: any) {
+            return [];
+        }
+    }
+
+    async findOne(query: FilterQuery<T>): Promise<T | null> {
+        try {
+            return await this.model.findOne(query);
+        } catch (err: any) {
+            return null;
+        }
+    }
+
+
+    async create(data: Partial<T>): Promise<T | null> {
+        try {
+            const createdDocument = new this.model(data);
+            return await createdDocument.save();
+        } catch (err: any) {
+            return null;
+        }
+    }
+
+
+    async update(query: FilterQuery<T>, data: UpdateQuery<T>, option?: QueryOptions): Promise<T | null> {
+        try {
+            const updatedDocument = await this.model.findOneAndUpdate(query, data, option? option : {});
+            return updatedDocument;
+        } catch (err: any) {
+            return null;
+        }
+    }
+
+    async delete(query: FilterQuery<T>): Promise<T | null> {
+        try {
+            const deletedDocument = await this.model.findOneAndDelete(query);
+            return deletedDocument;
+        } catch (err: any) {
+            return null;
+        }
+    }
+}
